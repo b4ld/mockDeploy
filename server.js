db516964faf9c8a4fdf5971f9e6d170dc3863962
pedro_carmezim <pedro.carmezim@novabase.pt>
2019-09-11 19:52:37 +0100

    add send logs button

--- server.js
+++ server.js
@@ -4,22 +4,26 @@ const request = require('request')
 const app = express()
 const bodyParser = require('body-parser');
 
-
-
-
 const portt = 3999
 
 app.use(express.json());
 app.use(express.urlencoded())
 app.use(express.static("public_index"));
 app.listen(portt, () => console.log('App listening on portt ' + portt))
+var d = new Date();
+var k = 0; 
 
-
-app.get('/test', (req, res) => {
-        
-        res.json({requestBody: req.body})
-      
- })
+app.get('/logs', (req, res) => {
+        console.log("Time is --- " + d)
+        var i = 0;
+        while (i < 10) {
+                console.log("We made " + i + " --Iterations ----- find the Word RED HAMMER ");
+                i++;
+        }
+        console.log("Times Pressed " + k)
+        k++
+        res.redirect("/")
+})
 
 
 
